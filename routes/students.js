68494854ab7618296f3dc0601b0694c28a23e59b
Aswin Nair <aswintorch@gmail.com>
2020-06-14 19:16:08 -0400

    Added route to fetch single student

--- routes/students.js
+++ routes/students.js
@@ -32,6 +32,25 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
+// Route to serve single student based on its id
+// /api/students/:id
+// /api/students/1 would respond with a student with id 1
+router.get("/:id", async (req, res, next) => {
+  // take the id from params
+  const { id } = req.params;
+  // query the database for a student with matching id
+  try {
+    // if successful:
+    const student = await Student.findByPk(id);
+    // send back the student as a response
+    res.status(200).json(student);
+  } catch (err) {
+    // if error:
+    // handle error
+    next(err);
+  }
+});
+
 // Route to handle adding a student
 // /api/students/
 router.post("/", async (req, res, next) => {
@@ -52,6 +71,24 @@ router.post("/", async (req, res, next) => {
     // The database would return a student
     // send that student as a json to the client
     res.status(201).send(newStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route to handle removing a student
+// /api/students/:id
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  // get an id for a student to delete
+  try {
+    // pass the id to the database to find student to be deleted
+    // database would either respond succcess or fail
+    const student = await Student.findByPk(id);
+    // invoke the .destroy() method on the returned student
+    await student.destroy();
+    // send a success message to the client
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
